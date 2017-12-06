184dab6026fecdf1885d459beda46390dd66a645
gradzka <monika.gradzka@tlen.pl>
2017-12-07 00:19:26 +0100

    Pobieranie pracowników (z wyłączeniem siebie), pobieranie informacji o sobie, przełączanie między skrzynką nadawczą, a wysyłaniem nowej wiad
    omości, poprawka GUI

--- ClientApp/ClientApp/Form1.Designer.cs
+++ ClientApp/ClientApp/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.BGetMessagess = new System.Windows.Forms.Button();
             this.BSendMessages = new System.Windows.Forms.Button();
             this.BRevokeTheCertificate = new System.Windows.Forms.Button();
-            this.BGetTheCertificate = new System.Windows.Forms.Button();
             this.BApplyForTheCertificate = new System.Windows.Forms.Button();
             this.PSendMessage = new System.Windows.Forms.Panel();
             this.LTopicR = new System.Windows.Forms.Label();
@@ -46,9 +45,12 @@
             this.PGetMessage = new System.Windows.Forms.Panel();
             this.LTopicS = new System.Windows.Forms.Label();
             this.TBTopicS = new System.Windows.Forms.TextBox();
-            this.TBRecipient = new System.Windows.Forms.TextBox();
+            this.TBSender = new System.Windows.Forms.TextBox();
             this.TBMessageS = new System.Windows.Forms.TextBox();
             this.TBMessageR = new System.Windows.Forms.TextBox();
+            this.Topic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompanyEMail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MessageID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.DGVMessagesR)).BeginInit();
             this.PSendMessage.SuspendLayout();
             this.PGetMessage.SuspendLayout();
@@ -56,28 +58,42 @@
             // 
             // DGVMessagesR
             // 
+            this.DGVMessagesR.AllowUserToAddRows = false;
+            this.DGVMessagesR.AllowUserToDeleteRows = false;
+            this.DGVMessagesR.AllowUserToResizeColumns = false;
+            this.DGVMessagesR.AllowUserToResizeRows = false;
             this.DGVMessagesR.BackgroundColor = System.Drawing.Color.White;
             this.DGVMessagesR.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.DGVMessagesR.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVMessagesR.Location = new System.Drawing.Point(178, 98);
+            this.DGVMessagesR.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MessageID,
+            this.CompanyEMail,
+            this.Topic});
+            this.DGVMessagesR.Location = new System.Drawing.Point(35, 154);
+            this.DGVMessagesR.MultiSelect = false;
             this.DGVMessagesR.Name = "DGVMessagesR";
-            this.DGVMessagesR.Size = new System.Drawing.Size(265, 338);
+            this.DGVMessagesR.ReadOnly = true;
+            this.DGVMessagesR.RowHeadersVisible = false;
+            this.DGVMessagesR.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DGVMessagesR.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGVMessagesR.Size = new System.Drawing.Size(265, 282);
             this.DGVMessagesR.TabIndex = 9;
-            this.toolTip.SetToolTip(this.DGVMessagesR, "Kliknij dwukrotnie na wiadomość, by odczytać");
+            this.toolTip.SetToolTip(this.DGVMessagesR, "\r\n");
             this.DGVMessagesR.Visible = false;
+            this.DGVMessagesR.SelectionChanged += new System.EventHandler(this.SelectionChanged);
             // 
             // BGetMessagess
             // 
-            this.BGetMessagess.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BGetMessagess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.BGetMessagess.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BGetMessagess.FlatAppearance.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.BGetMessagess.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.BGetMessagess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BGetMessagess.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F);
             this.BGetMessagess.ForeColor = System.Drawing.Color.Black;
-            this.BGetMessagess.Location = new System.Drawing.Point(12, 356);
+            this.BGetMessagess.Location = new System.Drawing.Point(34, 98);
             this.BGetMessagess.Name = "BGetMessagess";
-            this.BGetMessagess.Size = new System.Drawing.Size(140, 80);
-            this.BGetMessagess.TabIndex = 4;
+            this.BGetMessagess.Size = new System.Drawing.Size(131, 40);
+            this.BGetMessagess.TabIndex = 1;
             this.BGetMessagess.Text = "Skrzynka odbiorcza";
             this.toolTip.SetToolTip(this.BGetMessagess, "Skrzynka odbiorcza");
             this.BGetMessagess.UseVisualStyleBackColor = false;
@@ -91,11 +107,11 @@
             this.BSendMessages.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BSendMessages.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F);
             this.BSendMessages.ForeColor = System.Drawing.Color.Black;
-            this.BSendMessages.Location = new System.Drawing.Point(12, 270);
+            this.BSendMessages.Location = new System.Drawing.Point(173, 97);
             this.BSendMessages.Name = "BSendMessages";
-            this.BSendMessages.Size = new System.Drawing.Size(140, 80);
-            this.BSendMessages.TabIndex = 3;
-            this.BSendMessages.Text = "Skrzynka nadawcza";
+            this.BSendMessages.Size = new System.Drawing.Size(131, 41);
+            this.BSendMessages.TabIndex = 2;
+            this.BSendMessages.Text = "Nowa wiadomość";
             this.toolTip.SetToolTip(this.BSendMessages, "Skrzynka nadawcza");
             this.BSendMessages.UseVisualStyleBackColor = false;
             this.BSendMessages.Click += new System.EventHandler(this.BSendMessages_Click);
@@ -108,31 +124,14 @@
             this.BRevokeTheCertificate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BRevokeTheCertificate.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F);
             this.BRevokeTheCertificate.ForeColor = System.Drawing.Color.Black;
-            this.BRevokeTheCertificate.Location = new System.Drawing.Point(12, 184);
+            this.BRevokeTheCertificate.Location = new System.Drawing.Point(450, 97);
             this.BRevokeTheCertificate.Name = "BRevokeTheCertificate";
-            this.BRevokeTheCertificate.Size = new System.Drawing.Size(140, 80);
-            this.BRevokeTheCertificate.TabIndex = 2;
+            this.BRevokeTheCertificate.Size = new System.Drawing.Size(131, 40);
+            this.BRevokeTheCertificate.TabIndex = 4;
             this.BRevokeTheCertificate.Text = "Unieważnij certyfikat";
             this.toolTip.SetToolTip(this.BRevokeTheCertificate, "Unieważnij certyfikat");
             this.BRevokeTheCertificate.UseVisualStyleBackColor = false;
             this.BRevokeTheCertificate.Click += new System.EventHandler(this.BRevokeTheCertificate_Click);
-            // 
-            // BGetTheCertificate
-            // 
-            this.BGetTheCertificate.BackColor = System.Drawing.Color.Gold;
-            this.BGetTheCertificate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BGetTheCertificate.FlatAppearance.BorderColor = System.Drawing.Color.DarkGoldenrod;
-            this.BGetTheCertificate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BGetTheCertificate.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F);
-            this.BGetTheCertificate.ForeColor = System.Drawing.Color.Black;
-            this.BGetTheCertificate.Location = new System.Drawing.Point(12, 98);
-            this.BGetTheCertificate.Name = "BGetTheCertificate";
-            this.BGetTheCertificate.Size = new System.Drawing.Size(140, 80);
-            this.BGetTheCertificate.TabIndex = 1;
-            this.BGetTheCertificate.Text = "Pobierz certyfikat";
-            this.toolTip.SetToolTip(this.BGetTheCertificate, "Pobierz certyfikat");
-            this.BGetTheCertificate.UseVisualStyleBackColor = false;
-            this.BGetTheCertificate.Click += new System.EventHandler(this.BGetTheCertificate_Click);
             // 
             // BApplyForTheCertificate
             // 
@@ -142,10 +141,10 @@
             this.BApplyForTheCertificate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BApplyForTheCertificate.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.BApplyForTheCertificate.ForeColor = System.Drawing.Color.Black;
-            this.BApplyForTheCertificate.Location = new System.Drawing.Point(12, 12);
+            this.BApplyForTheCertificate.Location = new System.Drawing.Point(311, 97);
             this.BApplyForTheCertificate.Name = "BApplyForTheCertificate";
-            this.BApplyForTheCertificate.Size = new System.Drawing.Size(140, 80);
-            this.BApplyForTheCertificate.TabIndex = 0;
+            this.BApplyForTheCertificate.Size = new System.Drawing.Size(131, 40);
+            this.BApplyForTheCertificate.TabIndex = 3;
             this.BApplyForTheCertificate.Text = "Złóż wniosek \r\no wydanie certyfikatu";
             this.toolTip.SetToolTip(this.BApplyForTheCertificate, "Złóż wniosek o wydanie certyfikatu");
             this.BApplyForTheCertificate.UseVisualStyleBackColor = false;
@@ -159,7 +158,7 @@
             this.PSendMessage.Controls.Add(this.BSend);
             this.PSendMessage.Controls.Add(this.CBRecNames);
             this.PSendMessage.Controls.Add(this.LRecipient);
-            this.PSendMessage.Location = new System.Drawing.Point(177, 12);
+            this.PSendMessage.Location = new System.Drawing.Point(34, 12);
             this.PSendMessage.Name = "PSendMessage";
             this.PSendMessage.Size = new System.Drawing.Size(559, 80);
             this.PSendMessage.TabIndex = 5;
@@ -232,8 +231,8 @@
             this.PGetMessage.Controls.Add(this.LTopicS);
             this.PGetMessage.Controls.Add(this.TBTopicS);
             this.PGetMessage.Controls.Add(this.LSender);
-            this.PGetMessage.Controls.Add(this.TBRecipient);
-            this.PGetMessage.Location = new System.Drawing.Point(177, 12);
+            this.PGetMessage.Controls.Add(this.TBSender);
+            this.PGetMessage.Location = new System.Drawing.Point(34, 12);
             this.PGetMessage.Name = "PGetMessage";
             this.PGetMessage.Size = new System.Drawing.Size(559, 80);
             this.PGetMessage.TabIndex = 6;
@@ -257,33 +256,54 @@
             this.TBTopicS.Size = new System.Drawing.Size(476, 20);
             this.TBTopicS.TabIndex = 3;
             // 
-            // TBRecipient
+            // TBSender
             // 
-            this.TBRecipient.Location = new System.Drawing.Point(68, 15);
-            this.TBRecipient.Name = "TBRecipient";
-            this.TBRecipient.ReadOnly = true;
-            this.TBRecipient.Size = new System.Drawing.Size(476, 20);
-            this.TBRecipient.TabIndex = 2;
+            this.TBSender.Location = new System.Drawing.Point(68, 15);
+            this.TBSender.Name = "TBSender";
+            this.TBSender.ReadOnly = true;
+            this.TBSender.Size = new System.Drawing.Size(476, 20);
+            this.TBSender.TabIndex = 2;
             // 
             // TBMessageS
             // 
-            this.TBMessageS.Location = new System.Drawing.Point(178, 98);
+            this.TBMessageS.Location = new System.Drawing.Point(35, 154);
             this.TBMessageS.Multiline = true;
             this.TBMessageS.Name = "TBMessageS";
-            this.TBMessageS.Size = new System.Drawing.Size(545, 338);
+            this.TBMessageS.Size = new System.Drawing.Size(545, 282);
             this.TBMessageS.TabIndex = 7;
             // 
             // TBMessageR
             // 
             this.TBMessageR.Enabled = false;
-            this.TBMessageR.Location = new System.Drawing.Point(458, 98);
+            this.TBMessageR.Location = new System.Drawing.Point(315, 154);
             this.TBMessageR.Multiline = true;
             this.TBMessageR.Name = "TBMessageR";
             this.TBMessageR.ReadOnly = true;
-            this.TBMessageR.Size = new System.Drawing.Size(265, 338);
+            this.TBMessageR.Size = new System.Drawing.Size(265, 282);
             this.TBMessageR.TabIndex = 10;
             this.TBMessageR.Visible = false;
             this.TBMessageR.WordWrap = false;
+            // 
+            // Topic
+            // 
+            this.Topic.HeaderText = "Temat";
+            this.Topic.Name = "Topic";
+            this.Topic.ReadOnly = true;
+            this.Topic.Width = 145;
+            // 
+            // CompanyEMail
+            // 
+            this.CompanyEMail.HeaderText = "Nadawca";
+            this.CompanyEMail.Name = "CompanyEMail";
+            this.CompanyEMail.ReadOnly = true;
+            // 
+            // MessageID
+            // 
+            this.MessageID.HeaderText = "MessageID";
+            this.MessageID.Name = "MessageID";
+            this.MessageID.ReadOnly = true;
+            this.MessageID.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.MessageID.Visible = false;
             // 
             // Form1
             // 
@@ -292,7 +312,7 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = global::ClientApp.Properties.Resources.background2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(746, 457);
+            this.ClientSize = new System.Drawing.Size(615, 466);
             this.Controls.Add(this.TBMessageR);
             this.Controls.Add(this.DGVMessagesR);
             this.Controls.Add(this.PSendMessage);
@@ -301,7 +321,6 @@
             this.Controls.Add(this.BGetMessagess);
             this.Controls.Add(this.BSendMessages);
             this.Controls.Add(this.BRevokeTheCertificate);
-            this.Controls.Add(this.BGetTheCertificate);
             this.Controls.Add(this.BApplyForTheCertificate);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -323,7 +342,6 @@
         #endregion
 
         private System.Windows.Forms.Button BApplyForTheCertificate;
-        private System.Windows.Forms.Button BGetTheCertificate;
         private System.Windows.Forms.Button BRevokeTheCertificate;
         private System.Windows.Forms.Button BSendMessages;
         private System.Windows.Forms.Button BGetMessagess;
@@ -335,13 +353,16 @@
         private System.Windows.Forms.Panel PGetMessage;
         private System.Windows.Forms.Label LTopicS;
         private System.Windows.Forms.TextBox TBTopicS;
-        private System.Windows.Forms.TextBox TBRecipient;
+        private System.Windows.Forms.TextBox TBSender;
         private System.Windows.Forms.Label LRecipient;
         private System.Windows.Forms.TextBox TBMessageS;
         private System.Windows.Forms.Button BSend;
         private System.Windows.Forms.Label LTopicR;
         private System.Windows.Forms.DataGridView DGVMessagesR;
         private System.Windows.Forms.TextBox TBMessageR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MessageID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CompanyEMail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Topic;
     }
 }
 
--- ClientApp/ClientApp/Form1.cs
+++ ClientApp/ClientApp/Form1.cs
@@ -11,13 +11,70 @@ using System.Windows.Forms;
 
 namespace ClientApp
 {
+    //TODO: zabezpiecz bledy - wyjscie po msgbox
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            BGetMessagess_Click(null, null);
+            GetMyInfo();
+            SetCBRecNames(); //Combobox with employeees you send message
+            SetDGVMessageR(); //Data Grid View where you can check employee to read messages
+            SelectionChanged(null, null);
         }
 
+        private Employee infoAboutMe = new Employee();
+
+        private void GetMyInfo()
+        {
+            var request = new RestRequest("api/employee/me/", Method.GET);
+            request.AddHeader("Authorization", "Token " + Program.token);
+            IRestResponse<Employee> response = Program.client.Execute<Employee>(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                infoAboutMe.id = response.Data.id;
+                infoAboutMe.name = response.Data.name;
+                infoAboutMe.surname = response.Data.surname;
+                infoAboutMe.pesel = response.Data.pesel;
+                infoAboutMe.address = response.Data.address;
+                infoAboutMe.birth_day = response.Data.birth_day;
+                infoAboutMe.job_id = response.Data.job_id;
+                infoAboutMe.company_email = response.Data.company_email;
+            }
+            else
+            {
+                MessageBox.Show("Błąd wewnętrzny aplikacji, skontaktuj się z administratorem", "Błąd!");
+            }
+        }
+        private void SetDGVMessageR() //Fill Inbox
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                var index = DGVMessagesR.Rows.Add();
+                DGVMessagesR.Rows[index].Cells[0].Value = i;
+                DGVMessagesR.Rows[index].Cells[1].Value = "karol.koralin@koralowo.pl";
+                DGVMessagesR.Rows[index].Cells[2].Value = "Blah blah blah blah blah blah " + i;
+            }
+        }
+        private void SetCBRecNames()
+        {
+            //Get employees
+            var request = new RestRequest("api/employee/", Method.GET);
+            request.AddHeader("Authorization", "Token " + Program.token);
+            IRestResponse<List<Employee>> response = Program.client.Execute<List<Employee>>(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                CBRecNames.ValueMember = "id"; //EmployeeID
+                CBRecNames.DisplayMember = "company_email";
+                CBRecNames.DataSource = response.Data.Where(x=>x.id != infoAboutMe.id).ToList();
+            }
+            else
+            {
+                MessageBox.Show("Błąd wewnętrzny aplikacji, skontaktuj się z administratorem", "Błąd!");
+            }
+        }
         private void BApplyForTheCertificate_Click(object sender, EventArgs e)
         {
 
@@ -41,6 +98,8 @@ namespace ClientApp
             TBMessageS.Visible = true;
             DGVMessagesR.Visible = false;
             TBMessageR.Visible = false;
+
+            //Get emplo
         }
 
         private void BGetMessagess_Click(object sender, EventArgs e)
@@ -63,8 +122,30 @@ namespace ClientApp
                 e.Cancel = true;
                 MessageBox.Show("Klient nie może zostać wylogowany, skontaktuj się z administratorem systemu!", "Błąd!");
             }
-
-
         }
+
+        private void SelectionChanged(object sender, EventArgs e)
+        {
+            var rowsCount = DGVMessagesR.SelectedRows.Count;
+            if (rowsCount <1) return; //if 0 Messages
+            var row = DGVMessagesR.SelectedRows[0];
+            string senderTB = row.Cells[1].Value.ToString();
+            string topicTB = row.Cells[2].Value.ToString();
+            TBSender.Text = senderTB;
+            TBTopicS.Text = topicTB;
+        }
+    }
+
+    
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string pesel { get; set; }
+        public string address { get; set; }
+        public string birth_day { get; set; }
+        public int job_id { get; set; }
+        public string company_email { get; set; }
     }
 }
